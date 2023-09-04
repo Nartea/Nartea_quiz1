a95fe0dff4cac87db58ab8f1c8559dc67d33462d
Acer <Acer@LAPTOP-FR1NH8EI.mshome.net>
2023-09-04 16:22:24 +0800

    ako si csar

---
+++ src/nartea_quiz1/Nartea_quiz1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nartea_quiz1;
+
+/**
+ *
+ * @author Acer
+ */
+public class Nartea_quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
